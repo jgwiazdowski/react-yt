18ece74affa49d962aca5130e20cb2af83547917
Jakub Gwiazdowski <jakub.gwiazdowski@hotmail.com>
2018-05-25 18:24:02 +0200

    hey

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
+
+const API_KEY = 'AIzaSyBU7cQYJFFlle9k9BIFQwiEAkYmD_6_ZbU';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      videos: []
+    }
+
+    YTSearch({ key: API_KEY, term: 'funny cucumber' }, (videos) => {
+      this.setState({ videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container')
+)
